1257152299b50e23aa28aec7538ef2c830060cf1
Iskandirov <Iskandirov@A33COMP13>
2019-03-28 20:55:16 +0200

    create a normal about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  ownerName = "Maks";
-  languages = ['C#', 'C++', 'Pascal', 'Vasik', 'Assembler'];
+  ownerName = "Ivanov Vasya Pypkin ";
+  workplace = "Programmer";
+  phonenumber = "+8805553535";
+  email = "vasyapypkinthebest@gmail.com";
+  age = "28";
+  Living = "v.Zahalystie";
+  WantedSalary = "999999999$";
+  citizenship = "Ukraine";
+  Language = "Russian(good),Ukrainian(native),English(colloquial)";
   constructor() { }
 
   ngOnInit() {
